cf905fd11036b7839ac929c378fe9cc748f8ac9b
Hausch Johannes <jh285@hdm-stuttgart.de>
2022-10-25 17:36:03 +0200

    Add ImageSlider

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -7,13 +7,21 @@ import {
 } from "react-router-dom";
 
 import Navbar from './navbar/Navbar';
+import ImageSlider from './recipe-full-view/image-slider/ImageSlider';
+
+const slides = [
+    {url: "./image-01.jpg"},
+    {url: "./image-02.jpg"},
+    {url: "./image-03.jpg"},
+    {url: "./image-04.jpeg"}
+]
 
 function App() {
   return (
     <div>
       <Navbar/>
       <HashRouter>
-
+        <ImageSlider slides={slides}/>
       </HashRouter>
     </div>
   );
