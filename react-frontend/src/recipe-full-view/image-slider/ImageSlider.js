c731f1da9dc4d1f4aa04563d9304187f265755b1
Kay Knöpfle <kay.knoepfle@gmx.de>
2022-10-26 18:12:36 +0200

    fixedimage gradient

--- react-frontend/src/recipe-full-view/image-slider/ImageSlider.js
+++ react-frontend/src/recipe-full-view/image-slider/ImageSlider.js
@@ -15,7 +15,7 @@ export default function ImageSlider({slides}) {
     }
 
     const slideImage = {
-        backgroundImage: `url(${slides[currentIndex].url})`,
+        backgroundImage: `linear-gradient(to bottom, rgba(245, 246, 252, 0.52), rgba(117, 19, 93, 0.73)), url(${props.slides[currentIndex].url})`,
     }
 
     const setSliderButtons = slides.map(
