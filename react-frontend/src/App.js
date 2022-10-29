90f61d7521f4bb7754f1d32e51318e0aba49cfd2
dodo <ds217@hdm-stuttgart.de>
2022-10-29 13:33:59 +0200

    add RecipeEditor

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -7,11 +7,13 @@ import {
 } from "react-router-dom";
 
 import Navbar from './navbar/Navbar';
+import RecipeEditor from "./recipe-editor/RecipeEditor";
 
 function App() {
   return (
     <div>
       <Navbar/>
+       <RecipeEditor/>
       <BrowserRouter>
       </BrowserRouter>
     </div>
