8b86be6e96bfd53913a25499955079e4cd5794de
Hausch Johannes <jh285@hdm-stuttgart.de>
2022-10-29 14:05:27 +0200

    Change style difficulty

--- react-frontend/src/recipe-editor/RecipeEditor.js
+++ react-frontend/src/recipe-editor/RecipeEditor.js
@@ -4,22 +4,23 @@ import image_01 from "../recipe-full-view/image-slider/image-01.jpg";
 import image_02 from "../recipe-full-view/image-slider/image-02.jpg";
 import image_03 from "../recipe-full-view/image-slider/image-03.jpg";
 import UploadedImages from "./uploadedImages";
+import {useState} from "react";
 
 export default function RecipeEditor() {
 
 
-    const demoIngredient = [
-        {
-            amount: 250,
-            unit: 'gr',
-            ingredient: 'Murloc',
-        },
-        {
-            amount: 250000,
-            unit: 'Hundepfoten',
-            ingredient: 'Murloc',
-        }
-    ]
+    // const demoIngredient = [
+    //     {
+    //         amount: 250,
+    //         unit: 'gr',
+    //         ingredient: 'Murloc',
+    //     },
+    //     {
+    //         amount: 250000,
+    //         unit: 'Hundepfoten',
+    //         ingredient: 'Murloc',
+    //     }
+    // ]
 
 
     const slides = [
@@ -28,19 +29,10 @@ export default function RecipeEditor() {
         {url: image_03},
     ]
 
-    var difficulty2 = 0;
+    const [difficulty, setDifficulty] = useState(0);
 
     const switchDifficulty = (difficulty) =>{
-        difficulty2 = difficulty;
-        console.log(difficulty2)
-            let symbols = document.getElementsByClassName("symbols-difficulty");
-            for (let i = 0; i < symbols.length; i++){
-                if (difficulty2 >= i){
-                    symbols[i].style.color = "black";
-                } else {
-                    symbols[i].style.color = "lightgrey";
-                }
-            }
+        setDifficulty(difficulty);
         }
 
 
@@ -58,10 +50,9 @@ export default function RecipeEditor() {
                 </div>
                 <div className={styles.informationContainer}>
                     <div className={styles.informationContainer}>
-
-                        <span className={`material-symbols-outlined ` + styles.symbolsDifficulty} onClick={() => switchDifficulty(0)} style={{color: difficulty2 > 0 ? 'white' : 'powderblue'}}>lunch_dining</span>
-                        <span className={`material-symbols-outlined ` + styles.symbolsDifficulty} onClick={() => switchDifficulty(1)} style={{color: difficulty2 > 1 ? 'white' : 'powderblue'}}>lunch_dining</span>
-                        <span className={`material-symbols-outlined ` + styles.symbolsDifficulty} onClick={() => switchDifficulty(2)} style={{color: difficulty2 > 2 ? 'white' : 'powderblue'}}>lunch_dining</span>
+                        <span className={`material-symbols-outlined ` + styles.symbolsDifficulty} style={difficulty >= 1 ? {color: 'black'} : {color: 'lightgray'}} onClick={() => switchDifficulty(1)}>lunch_dining</span>
+                        <span className={`material-symbols-outlined ` + styles.symbolsDifficulty} style={difficulty >= 2 ? {color: 'black'} : {color: 'lightgray'}} onClick={() => switchDifficulty(2)}>lunch_dining</span>
+                        <span className={`material-symbols-outlined ` + styles.symbolsDifficulty} style={difficulty >= 3 ? {color: 'black'} : {color: 'lightgray'}} onClick={() => switchDifficulty(3)}>lunch_dining</span>
                         Schwierigkeit
                     </div>
                     <div className={styles.informationContainer}>
