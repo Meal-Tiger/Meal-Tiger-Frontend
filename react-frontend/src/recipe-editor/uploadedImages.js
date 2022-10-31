b294f0b7352a6687f22f75c7da83eec1fcf3066e
dodo <ds217@hdm-stuttgart.de>
2022-10-31 10:00:00 +0100

    RecipeEditor file upload and input fields added

--- react-frontend/src/recipe-editor/uploadedImages.js
+++ react-frontend/src/recipe-editor/uploadedImages.js
@@ -1,16 +1,44 @@
 import styles from './RecipeEditor.module.css';
+import {useState} from "react";
 
-export default function UploadedImages ({images}){
+export default function UploadedImages({images}) {
 
-    const getSmallImage = (images) => {
-            images.map( (image) => {
-                return <div style={{backgroundImage:`url(${image.url})`}}></div>
-            })
+    const [files, setFiles] = useState([]);
+    const [firstPreview, setFirstPreview] = useState();
+
+    function handleChange(e) {
+        setFiles(files => []);
+        if (e.target.files.length === 0) {
+            return;
+        }
+        setFirstPreview(firstPreview => [
+                firstPreview = URL.createObjectURL(e.target.files[0])
+            ]
+        )
+        for (let i = 1; i < e.target.files.length; i++) {
+            setFiles(files => [
+                ...files,
+                URL.createObjectURL(e.target.files[i])
+            ]);
+        }
     }
 
-    return(
-        <div className={styles.rightImages}>
-            {getSmallImage(images)}
+    return (
+        <div className={styles.imageContainer}>
+            <div className={styles.previewImageContainer}>
+                <div className={styles.firstPreviewImage}>
+                    <img src={firstPreview}/>
+                </div>
+                <div className={styles.previewImagesRight}>
+                    {files.map((file) =>
+                        <img className={styles.previewImage} src={file}/>
+                    )}
+                </div>
+            </div>
+            <div>
+                <label htmlFor={styles.uploadImage} className={styles.uploadImageLabel}>+</label>
+                <input id={styles.uploadImage} multiple={true} type='file' onChange={handleChange}/>
+            </div>
         </div>
     )
 }
