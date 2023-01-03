d5efdf672b807e383806e384bd69bd8139ca5087
Hausch Johannes <jh285@hdm-stuttgart.de>
2023-01-03 12:36:20 +0100

    Change the recipe image to load it as a background property Use the custom css classes
    Co-authored-by: DorinaSobiecki <115489365+dorinasobiecki@users.noreply.github.com>

--- react-frontend/src/RecipeOverview/RecipeCard/RecipeCard.js
+++ react-frontend/src/RecipeOverview/RecipeCard/RecipeCard.js
@@ -6,24 +6,29 @@ import user from './user.png';
 export default function RecipeCard(props) {
 
 	return (
-		<div onClick={props.onClick} className={styles.card}>
-			<img className={styles.image} src={essen} alt=""></img>
-			<div className={styles.infobox}>
+		<div onClick={props.onClick} className={styles["recipe-card"]}>
+			<div
+				title = {"RecipeImage"} //TODO make this changeable
+				className={`${styles["recipe-card-img"]}`}
+				style={{ backgroundImage: `url(${essen})` }}
+				>
+			</div>
+			<div className={styles["recipe-card-infobox"]}>
 				<h1 className="titel">{props.title}</h1>
-				<div className="time">
+				<div className={styles["recipe-card-info"]}>
 					<span className="material-symbols-outlined">schedule</span>
 					{props.time} min.
 				</div>
-				<div className="difficulty">
+				<div className={`${styles["recipe-card-info"]} ${"difficulty"}`}>
 					<span className="material-symbols-outlined">lunch_dining</span>
 					<span className="material-symbols-outlined">lunch_dining</span>
 					<span className="material-symbols-outlined">lunch_dining</span>
 				</div>
-				<div className={styles.user}>
+				<div className={` ${styles["recipe-card-info"]} ${styles.user}`}>
 					<img className={styles.user} src={user} alt=""></img> Benutzername
 				</div>
-				<div className="rating">
-					<span class="material-symbols-outlined">star</span>
+				<div className={`${styles["recipe-card-info"]} ${"rating"}`}>
+					<span className={"material-symbols-outlined"}>star</span>
 					{props.rating}/5
 				</div>
 			</div>
