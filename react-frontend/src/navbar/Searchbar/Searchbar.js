4bb4ab570ccbc9165f899dbd636e6adba10a7e4a
ds217 <ds217@hdm-stuttgart.de>
2023-01-03 17:14:31 +0100

    added custom css code for navbar #57
    Co-authored-by: JohannesHausch <50317380+johanneshausch@users.noreply.github.com>

--- react-frontend/src/navbar/Searchbar/Searchbar.js
+++ react-frontend/src/navbar/Searchbar/Searchbar.js
@@ -3,12 +3,10 @@ import styles from './Searchbar.module.css';
 
 function Searchbar(){
     return (
-        <div>
             <form className={styles.form}>
-                <input className={styles.input} type="text" placeholder="Search.."></input>
-                <button className={styles.button} type="submit"><span className="material-symbols-outlined">search</span></button>
+                <input type="text" placeholder="Search.."></input>
+                <button className={"btn btn-icon"} type="submit"><span className="material-symbols-outlined">search</span></button>
             </form>
-        </div>
         
     );
 }
