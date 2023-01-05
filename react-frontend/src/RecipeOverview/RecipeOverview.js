acb9ebf134db9f626ef579193b6a6a1bf3598bf4
Hausch Johannes <jh285@hdm-stuttgart.de>
2023-01-05 23:51:49 +0100

    Add before and next button to change between the pages. Add select-tag to choose how many recipes are displayed.
    Co-authored-by: Kay Knöpfle <35371434+joystick01@users.noreply.github.com>

--- react-frontend/src/RecipeOverview/RecipeOverview.js
+++ react-frontend/src/RecipeOverview/RecipeOverview.js
@@ -1,27 +1,62 @@
-import { useNavigate } from 'react-router-dom';
+import {useNavigate} from 'react-router-dom';
 
-import { useGetRecipePage } from '../modules/api';
+import {useGetRecipePage} from '../modules/api';
 
-import { useParams } from "react-router-dom";
+import {useParams} from 'react-router-dom';
 
-import styles from './RecipeOverview.module.css'
+import styles from './RecipeOverview.module.css';
 
 import RecipeCard from './RecipeCard/RecipeCard';
+import { useState } from 'react';
 
-export default function RecipeOverview(){
+export default function RecipeOverview() {
 
-    let navigate = useNavigate();
-    let {query} = useParams();
-    let recipes = useGetRecipePage({q: query});    
+    const [itemsPerPage, setItemsPerPage] = useState(15);
 
-    if (recipes) {
-        return (
-            <div className={styles["recipe-overview-container"]}>
-                {recipes.recipes.map( (recipe) => <RecipeCard key={recipe.id} onClick={() => navigate(`/recipe/${recipe.id}`)} {...recipe}/>)}
-            </div>
-        );
-    } else {
-        return (<div>loading</div>);
+	let navigate = useNavigate();
+	let {query, page} = useParams();
+	let recipes = useGetRecipePage({q: query, page: page, size: itemsPerPage});
+
+    function nextPage(){
+
+        if(query){
+            navigate(`/search/${query}/page/${recipes.currentPage + 1}`);
+        }
+        else {
+            navigate(`/page/${recipes.currentPage + 1}`);
+        }
     }
-    
+
+    const beforePage = () => {
+        if(query){
+            navigate(`/search/${query}/page/${recipes.currentPage - 1} `)
+        }
+        else{
+            navigate(`/page/${recipes.currentPage - 1}`)
+        }
+    }
+
+	if (recipes) {
+		return (
+			<div>
+				<div className={styles['recipe-overview-container']}>
+					{recipes.recipes.map((recipe) => (
+						<RecipeCard key={recipe.id} onClick={() => navigate(`/recipe/${recipe.id}`)} {...recipe} />
+					))}
+				</div>
+				<div className={styles['next-page-container']}>
+                    <select value={itemsPerPage} onChange={(event) => {setItemsPerPage(event.target.value)}}>
+                        <option>15</option>
+                        <option>50</option>
+                        <option>100</option>
+                    </select>
+                    {recipes.currentPage == 0 ? null : <span className="material-symbols-outlined" onClick={beforePage}>navigate_before</span>}
+                    <p>{`${recipes.currentPage +1} von ${recipes.totalPages}`}</p>
+                    {page == recipes.totalPages -1 ? null : <span className="material-symbols-outlined" onClick={nextPage}>navigate_next</span>}
+				</div>
+			</div>
+		);
+	} else {
+		return <div>loading</div>;
+	}
 }
