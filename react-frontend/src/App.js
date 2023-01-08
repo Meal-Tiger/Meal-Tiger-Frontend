7ddca42d4bee1eb42dfe5e2eb2e221623e34931e
Hausch Johannes <jh285@hdm-stuttgart.de>
2023-01-08 19:58:54 +0100

    Add: routes for Impressum and Datenschutz
    Co-authored-by: DorinaSobiecki <115489365+dorinasobiecki@users.noreply.github.com>

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -6,6 +6,9 @@ import Navbar from './navbar/Navbar';
 import RecipeOverview from './RecipeOverview/RecipeOverview';
 import RecipeFullView from './recipe-full-view/RecipeFullView';
 import RecipeEditor from 'recipe-editor/RecipeEditor';
+import Footer from "./footer/Footer";
+import SiteNotice from "./footer/SiteNotice/SiteNotice";
+import DataPrivacy from "./footer/DataPrivacy/DataPrivacy";
 
 function App() {
 
@@ -15,12 +18,15 @@ function App() {
 				<div className='main-content'>
 					<Routes>
 						<Route path="/" element={<RecipeOverview/>}/>
+						<Route path="/impressum" element={<SiteNotice/>}/>
+						<Route path="/datenschutz" element={<DataPrivacy/>}/>
 						<Route path="/page/:page" element={<RecipeOverview />}/>
 						<Route path="/recipe/:recipeId" element={<RecipeFullView />}/>
 						<Route path="/add-recipe" element={<RecipeEditor/>}/>
 						<Route path="/search/:query/page/:page" element={<RecipeOverview />}/>
 					</Routes>
 				</div>
+				<Footer/>
 			</BrowserRouter>
 	);
 }
