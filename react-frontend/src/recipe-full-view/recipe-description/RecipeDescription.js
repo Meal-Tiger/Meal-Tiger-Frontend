ae6d2fa43f3c7733db84c386efbe3b02101a839c
Hausch Johannes <jh285@hdm-stuttgart.de>
2023-01-17 18:52:54 +0100

    Add: error handling

--- react-frontend/src/recipe-full-view/recipe-description/RecipeDescription.js
+++ react-frontend/src/recipe-full-view/recipe-description/RecipeDescription.js
@@ -1,50 +1,56 @@
 import styles from './RecipeDescription.module.css';
 
-import IngredientsContainer from "./IngredientsContainer";
-import { useGetRecipe, getFormatedTime } from '../../modules/api';
-import { useParams } from "react-router-dom";
-import Throbber from "../../modules/throbber/throbber";
+import IngredientsContainer from './IngredientsContainer';
+import {useGetRecipe, getFormatedTime} from '../../modules/api';
+import {useParams} from 'react-router-dom';
+import Throbber from '../../modules/throbber/throbber';
+import Modal from "../../modules/Modal/Modal";
 
 export default function RecipeDescription() {
+	let {recipeId} = useParams();
+	let [recipe, error] = useGetRecipe(recipeId);
 
-    let { recipeId } = useParams();
-    let [recipe, error] = useGetRecipe(recipeId);
-    if(recipe)
-    {
-        return (
-            <div className={styles.descriptionGrid}>
-                <div className={styles.iconContainer}>
-                    <div className={styles.informationContainer}>
-                        <div className={styles.informationContainer}>
-                            <span className="material-symbols-outlined">person</span>
-                            Nutzer
-                        </div>
-                        <div className={styles.informationContainer}>
-                            <span className="material-symbols-outlined">star</span>
-                            {recipe.rating} / 5
-                        </div>
-                    </div>
-                    <div className={styles.informationContainer}>
-                        <div className={styles.informationContainer}>
-                            <span className={`material-symbols-outlined ${styles.iconDifficulty}`}>lunch_dining</span>
-                            <span className={`material-symbols-outlined ${styles.iconDifficulty} ${(recipe.difficulty >= 2 ? styles.active : styles.inactive)}`}>lunch_dining</span>
-                            <span className={`material-symbols-outlined ${styles.iconDifficulty} ${(recipe.difficulty >= 2 ? styles.active : styles.inactive)}`}>lunch_dining</span>
-                            Schwierigkeit
-                        </div>
-                        <div className={styles.informationContainer}>
-                            <span className="material-symbols-outlined">schedule</span>
-                            {getFormatedTime(recipe.time)}
-                        </div>
-                    </div>
-                </div>
-                <IngredientsContainer ingredientArray={recipe.ingredients}/>
-                <div>
-                    <h2>Rezeptbeschreibung</h2>
-                    <p>{recipe.description}</p>
-                </div>
-            </div>
-        )
-    } else {
-        return (<Throbber/>);
-    }
+	if (recipe) {
+		return (
+			<div className={styles.descriptionGrid}>
+				<div className={styles.iconContainer}>
+					<div className={styles.informationContainer}>
+						<div className={styles.informationContainer}>
+							<span className="material-symbols-outlined">person</span>
+							Nutzer
+						</div>
+						<div className={styles.informationContainer}>
+							<span className="material-symbols-outlined">star</span>
+							{recipe.rating} / 5
+						</div>
+					</div>
+					<div className={styles.informationContainer}>
+						<div className={styles.informationContainer}>
+							<span className={`material-symbols-outlined ${styles.iconDifficulty}`}>lunch_dining</span>
+							<span className={`material-symbols-outlined ${styles.iconDifficulty} ${recipe.difficulty >= 2 ? styles.active : styles.inactive}`}>lunch_dining</span>
+							<span className={`material-symbols-outlined ${styles.iconDifficulty} ${recipe.difficulty >= 2 ? styles.active : styles.inactive}`}>lunch_dining</span>
+							Schwierigkeit
+						</div>
+						<div className={styles.informationContainer}>
+							<span className="material-symbols-outlined">schedule</span>
+							{getFormatedTime(recipe.time)}
+						</div>
+					</div>
+				</div>
+				<IngredientsContainer ingredientArray={recipe.ingredients} />
+				<div>
+					<h2>Rezeptbeschreibung</h2>
+					<p>{recipe.description}</p>
+				</div>
+			</div>
+		);
+	} else if (error) {
+		return (
+			<Modal className="error" show={true}>
+				{error}
+			</Modal>
+		);
+	} else {
+		return <Throbber />;
+	}
 }
