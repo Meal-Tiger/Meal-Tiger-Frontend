0ef8470afc1e2c0116283d0580881d3ec52cb05b
ds217 <ds217@hdm-stuttgart.de>
2023-01-17 22:31:13 +0100

    shows success Modal when Recipe is saved #97

--- react-frontend/src/recipe-editor/RecipeEditor.js
+++ react-frontend/src/recipe-editor/RecipeEditor.js
@@ -6,6 +6,9 @@ import UploadedImages from './uploadedImages';
 import {createContext, useMemo, useState} from 'react';
 import IngredientsContainerEditable from './IngredientsContainerEditable';
 import {postRecipe} from 'modules/api';
+import {useEvent} from "../modules/events";
+import LoginWithKeycloak from "../navbar/Usermenu/LoginWithKeycloak/LoginWithKeycloak";
+import Modal from "../modules/Modal/Modal";
 
 export const RecipeContext = createContext({
     recipe: {
@@ -22,8 +25,11 @@ export const RecipeContext = createContext({
 
 export default function RecipeEditor() {
 
+
+    const [userIsLoggedIn, setUserIsLoggedIn] = useState(false)
     const slides = [{url: image_01}, {url: image_02}, {url: image_03}];
     const [time, setTime] = useState([0, 0]);
+    const [showSuccessMessage, setShowSuccessMessage] = useState(false);
     const [recipe, setRecipe] = useState({
         title: "",
         description: "",
@@ -32,6 +38,12 @@ export default function RecipeEditor() {
         time: 0,
         ingredients: []
     });
+
+    useEvent("login", () =>
+        {
+            setUserIsLoggedIn(true);
+        }
+    );
 
     const value = useMemo(
         () => ({recipe, setRecipe}),
@@ -42,76 +54,99 @@ export default function RecipeEditor() {
         setRecipe({...recipe, difficulty: difficulty});
     };
 
-    const handleSubmit = (event) => {
+    async function handleSubmit(event){
         event.preventDefault();
         setRecipe({...recipe, time: (parseInt(time[0])*60) + parseInt(time[1])});
-        let error = postRecipe(recipe);
-    };
+        let error = await postRecipe(recipe);
+        if (error == null){
+            setShowSuccessMessage(true)
+        }
+    }
 
-    return (
-        <RecipeContext.Provider value={value}>
-            <form onSubmit={handleSubmit}>
-                <div className={styles.container}>
-                    <div className={styles["left-column"]}>
-                        <div className={styles["recipe-input"]}>
-                            <label htmlFor={'recipeName'}>Rezeptname</label>
-                            <input name={'recipeName'} type={'text'} value={recipe.title} onChange={(event) => {
-                                setRecipe({...recipe, title: event.target.value})
-                            }}/>
-                        </div>
-                        <div className={styles["recipe-input"]}>
-                            <label htmlFor={'recipeDescription'}>Rezeptbeschreibung</label>
-                            <textarea name={'recipeDescription'} rows={4} value={recipe.description}
-                                      onChange={(event) => {
-                                          setRecipe({...recipe, description: event.target.value})
-                                      }}></textarea>
-                        </div>
-                        <button className={"btn btn-primary hide-mobile hide-tablet"} type="submit">
-                            Rezept speichern
-                        </button>
-                    </div>
 
-                    <div className={styles["right-column"]}>
-                        <UploadedImages images={slides}/>
-                        <div className={styles["information-container"]}>
-                            <div className={styles["information-container-child"]}>
+    if (!userIsLoggedIn){
+        return(
+            <Modal show={true} setShow={() => {return false;}}>
+                <h1>Login</h1>
+                <LoginWithKeycloak/>
+            </Modal>
+        );
+    }else {
+        return (
+            <RecipeContext.Provider value={value}>
+                <form onSubmit={handleSubmit}>
+                    <Modal show={showSuccessMessage} setShow={setShowSuccessMessage}>
+                        <h1>Rezept erfolgreich erstellt!</h1>
+                    </Modal>
+                    <div className={styles.container}>
+                        <div className={styles["left-column"]}>
+                            <div className={styles["recipe-input"]}>
+                                <label htmlFor={'recipeName'}>Rezeptname</label>
+                                <input name={'recipeName'} type={'text'} value={recipe.title} onChange={(event) => {
+                                    setRecipe({...recipe, title: event.target.value})
+                                }}/>
+                            </div>
+                            <div className={styles["recipe-input"]}>
+                                <label htmlFor={'recipeDescription'}>Rezeptbeschreibung</label>
+                                <textarea name={'recipeDescription'} rows={4} value={recipe.description}
+                                          onChange={(event) => {
+                                              setRecipe({...recipe, description: event.target.value})
+                                          }}></textarea>
+                            </div>
+                            <button className={"btn btn-primary hide-mobile hide-tablet"} type="submit">
+                                Rezept speichern
+                            </button>
+                        </div>
+
+                        <div className={styles["right-column"]}>
+                            <UploadedImages images={slides}/>
+                            <div className={styles["information-container"]}>
+                                <div className={styles["information-container-child"]}>
 							<span
                                 className={`material-symbols-outlined ${styles["symbols-difficulty"]}  ${styles.active}`}
                                 onClick={() => switchDifficulty(1)}>
 								lunch_dining
 							</span>
-                                <span
-                                    className={`material-symbols-outlined ${styles["symbols-difficulty"]}  ${recipe.difficulty >= 2 ? styles.active : styles.inactive}`}
-                                    onClick={() => switchDifficulty(2)}>
+                                    <span
+                                        className={`material-symbols-outlined ${styles["symbols-difficulty"]}  ${recipe.difficulty >= 2 ? styles.active : styles.inactive}`}
+                                        onClick={() => switchDifficulty(2)}>
 								lunch_dining
 							</span>
-                                <span
-                                    className={`material-symbols-outlined ${styles["symbols-difficulty"]}  ${recipe.difficulty >= 3 ? styles.active : styles.inactive}`}
-                                    onClick={() => switchDifficulty(3)}>
+                                    <span
+                                        className={`material-symbols-outlined ${styles["symbols-difficulty"]}  ${recipe.difficulty >= 3 ? styles.active : styles.inactive}`}
+                                        onClick={() => switchDifficulty(3)}>
 								lunch_dining
 							</span>
-                                <span>Schwierigkeit</span>
+                                    <span>Schwierigkeit</span>
+                                </div>
+                                <div className={styles["information-container-child"]}>
+                                    <span className="material-symbols-outlined">schedule</span>
+                                    <input name={'recipeTimeH'} className={styles["schedule-input"]} type={'number'}
+                                           value={time[0]} onChange={(event) => {
+                                        time[0] = event.target.value;
+                                        setTime([...time])
+                                    }}/>
+                                    <span className={styles["schedule-input-text"]}>Std. </span>
+                                    <input name={'recipeTimeM'} className={styles["schedule-input"]} type={'number'}
+                                           value={time[1]} onChange={(event) => {
+                                        time[1] = event.target.value;
+                                        setTime([...time])
+                                    }}/>
+                                    <span className={styles["schedule-input-text"]}>min.</span>
+                                </div>
                             </div>
-                            <div className={styles["information-container-child"]}>
-                                <span className="material-symbols-outlined">schedule</span>
-                                <input name={'recipeTimeH'} className={styles["schedule-input"]} type={'number'}
-                                       value={time[0]} onChange={(event) => {time[0] = event.target.value; setTime([...time])}}/>
-                                <span className={styles["schedule-input-text"]}>Std. </span>
-                                <input name={'recipeTimeM'} className={styles["schedule-input"]} type={'number'} value={time[1]} onChange={(event) => {time[1] = event.target.value; setTime([...time])}}/>
-                                <span className={styles["schedule-input-text"]}>min.</span>
+                            <div className={styles["ingredient-table-wrapper"]}>
+                                <IngredientsContainerEditable/>
                             </div>
+                            <button className={"btn btn-primary hide-desktop"} type="submit">
+                                Rezept speichern
+                            </button>
                         </div>
-                        <div className={styles["ingredient-table-wrapper"]}>
-                            <IngredientsContainerEditable/>
-                        </div>
-                        <button className={"btn btn-primary hide-desktop"} type="submit">
-                            Rezept speichern
-                        </button>
+
+
                     </div>
-
-
-                </div>
-            </form>
-        </RecipeContext.Provider>
-    );
+                </form>
+            </RecipeContext.Provider>
+        );
+    }
 }
