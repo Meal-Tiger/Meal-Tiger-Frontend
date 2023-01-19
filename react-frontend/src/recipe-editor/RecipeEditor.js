714219bcebecd5222b50b521d62bac7513d27832
Hausch Johannes <jh285@hdm-stuttgart.de>
2023-01-19 20:02:49 +0100

    Add: Error handling

--- react-frontend/src/recipe-editor/RecipeEditor.js
+++ react-frontend/src/recipe-editor/RecipeEditor.js
@@ -8,7 +8,7 @@ import IngredientsContainerEditable from './IngredientsContainerEditable';
 import {postRecipe} from 'modules/api';
 import Modal from '../modules/Modal/Modal';
 import {Navigate} from 'react-router-dom';
-import { openLoginModal_event } from 'modules/events';
+import {openLoginModal_event} from 'modules/events';
 
 export const RecipeContext = createContext({
 	recipe: {
@@ -23,11 +23,12 @@ export const RecipeContext = createContext({
 });
 
 export default function RecipeEditor() {
+	const [showModal, setShowModal] = useState(false);
+	const [error, setError] = useState(null);
 
-    useEffect(() => {
-        if (sessionStorage.getItem('login') === 'false') document.dispatchEvent(openLoginModal_event);
-    }, [])
-    
+	useEffect(() => {
+		if (sessionStorage.getItem('login') === 'false') document.dispatchEvent(openLoginModal_event);
+	}, []);
 
 	const slides = [{url: image_01}, {url: image_02}, {url: image_03}];
 	const [time, setTime] = useState([0, 0]);
@@ -43,31 +44,34 @@ export default function RecipeEditor() {
 
 	const value = useMemo(() => ({recipe, setRecipe}), [recipe]);
 
-    const switchDifficulty = (difficulty) => {
-        setRecipe({...recipe, difficulty: difficulty});
-    };
+	const switchDifficulty = (difficulty) => {
+		setRecipe({...recipe, difficulty: difficulty});
+	};
 
-    function setTimeToRecipe(event){
-        let value = event.target.value;
-        if (event.target.name === 'recipeTimeH') {
-            time[0] = value;
-        } else {
-            time[1] = value;
-        }
-        setTime([...time]);
-        setRecipe({...recipe, time: parseInt(time[0])*60+ parseInt(time[1])});
-    }
+	function setTimeToRecipe(event) {
+		let value = event.target.value;
+		if (event.target.name === 'recipeTimeH') {
+			time[0] = value;
+		} else {
+			time[1] = value;
+		}
+		setTime([...time]);
+		setRecipe({...recipe, time: parseInt(time[0]) * 60 + parseInt(time[1])});
+	}
 
-    async function handleSubmit(event){
-        event.preventDefault();
-        let [id, error] = await postRecipe(recipe);
-        if (error == null){
-            setShowSuccessMessage(true)
-        }
-    }
+	async function handleSubmit(event) {
+		event.preventDefault();
+		let [id, error] = await postRecipe(recipe);
+		if (error) {
+			setError(error);
+			setShowModal(true);
+		} else if (error == null) {
+			setShowSuccessMessage(true);
+		}
+	}
 
 	if (sessionStorage.getItem('login') === 'false') {
-		return (<Navigate to="/"/>);
+		return <Navigate to="/" />;
 	} else {
 		return (
 			<RecipeContext.Provider value={value}>
@@ -104,53 +108,58 @@ export default function RecipeEditor() {
 							</button>
 						</div>
 
-                        <div className={styles["right-column"]}>
-                            <UploadedImages images={slides}/>
-                            <div className={styles["information-container"]}>
-                                <div className={styles["information-container-child"]}>
-							<span
-                                className={`material-symbols-outlined ${styles["symbols-difficulty"]}  ${styles.active}`}
-                                onClick={() => switchDifficulty(1)}>
-								lunch_dining
-							</span>
-                                    <span
-                                        className={`material-symbols-outlined ${styles["symbols-difficulty"]}  ${recipe.difficulty >= 2 ? styles.active : styles.inactive}`}
-                                        onClick={() => switchDifficulty(2)}>
-								lunch_dining
-							</span>
-                                    <span
-                                        className={`material-symbols-outlined ${styles["symbols-difficulty"]}  ${recipe.difficulty >= 3 ? styles.active : styles.inactive}`}
-                                        onClick={() => switchDifficulty(3)}>
-								lunch_dining
-							</span>
-                                    <span>Schwierigkeit</span>
-                                </div>
-                                <div className={styles["information-container-child"]}>
-                                    <span className="material-symbols-outlined">schedule</span>
-                                    <input name={'recipeTimeH'} className={styles["schedule-input"]} type={'number'}
-                                           value={time[0]} onChange={(event) => {
-                                        setTimeToRecipe(event);
-                                    }}/>
-                                    <span className={styles["schedule-input-text"]}>Std. </span>
-                                    <input name={'recipeTimeM'} className={styles["schedule-input"]} type={'number'}
-                                           value={time[1]} onChange={(event) => {
-                                        setTimeToRecipe(event);
-                                    }}/>
-                                    <span className={styles["schedule-input-text"]}>min.</span>
-                                </div>
-                            </div>
-                            <div className={styles["ingredient-table-wrapper"]}>
-                                <IngredientsContainerEditable/>
-                            </div>
-                            <button className={"btn btn-primary hide-desktop"} type="submit">
-                                Rezept speichern
-                            </button>
-                        </div>
-
-
-                    </div>
-                </form>
-            </RecipeContext.Provider>
-        );
-    }
+						<div className={styles['right-column']}>
+							<UploadedImages images={slides} />
+							<div className={styles['information-container']}>
+								<div className={styles['information-container-child']}>
+									<span className={`material-symbols-outlined ${styles['symbols-difficulty']}  ${styles.active}`} onClick={() => switchDifficulty(1)}>
+										lunch_dining
+									</span>
+									<span className={`material-symbols-outlined ${styles['symbols-difficulty']}  ${recipe.difficulty >= 2 ? styles.active : styles.inactive}`} onClick={() => switchDifficulty(2)}>
+										lunch_dining
+									</span>
+									<span className={`material-symbols-outlined ${styles['symbols-difficulty']}  ${recipe.difficulty >= 3 ? styles.active : styles.inactive}`} onClick={() => switchDifficulty(3)}>
+										lunch_dining
+									</span>
+									<span>Schwierigkeit</span>
+								</div>
+								<div className={styles['information-container-child']}>
+									<span className="material-symbols-outlined">schedule</span>
+									<input
+										name={'recipeTimeH'}
+										className={styles['schedule-input']}
+										type={'number'}
+										value={time[0]}
+										onChange={(event) => {
+											setTimeToRecipe(event);
+										}}
+									/>
+									<span className={styles['schedule-input-text']}>Std. </span>
+									<input
+										name={'recipeTimeM'}
+										className={styles['schedule-input']}
+										type={'number'}
+										value={time[1]}
+										onChange={(event) => {
+											setTimeToRecipe(event);
+										}}
+									/>
+									<span className={styles['schedule-input-text']}>min.</span>
+								</div>
+							</div>
+							<div className={styles['ingredient-table-wrapper']}>
+								<IngredientsContainerEditable />
+							</div>
+							<button className={'btn btn-primary hide-desktop'} type="submit">
+								Rezept speichern
+							</button>
+						</div>
+					</div>
+				</form>
+				<Modal className="error" show={showModal} setShow={setShowModal}>
+					{error}
+				</Modal>
+			</RecipeContext.Provider>
+		);
+	}
 }
