0b917c8da442909eef158f6d6bbfb10189d9a935
Hausch Johannes <jh285@hdm-stuttgart.de>
2023-01-20 22:54:49 +0100

    Add: image-upload
    Co-authored-by: Kay Knöpfle <kay.knoepfle@gmx.de>

--- react-frontend/src/recipe-editor/uploadedImages.js
+++ react-frontend/src/recipe-editor/uploadedImages.js
@@ -1,40 +1,42 @@
 import styles from './RecipeEditor.module.css';
-import {useState} from "react";
+import {useContext} from "react";
+import {RecipeContext} from "./RecipeEditor";
 
-export default function UploadedImages({images}) {
+export default function UploadedImages() {
 
-    const [files, setFiles] = useState([]);
+    const {images, setImages} = useContext(RecipeContext);
 
     const handleChange = (event) => {
         if (event.target.files.length === 0) {
             return;
         }
         for (let i = 0; i < event.target.files.length; i++) {
-            setFiles(files => [
+            setImages(files => [
                 ...files,
                 URL.createObjectURL(event.target.files[i])
             ]);
+            console.log(images);
         }
     }
 
     const removeFile = (event) => {
         let removeId = event.target.getAttribute("data-index");
-        files.splice(removeId, 1);
-        setFiles([
-            ...files
+        images.splice(removeId, 1);
+        setImages([
+            ...images
         ]);
     }
 
     return (
-        <div className={`${styles["image-container"]} ${(files.length >= 1 ? styles["has-picture"] : styles["no-picture"])}`}>
+        <div className={`${styles["image-container"]} ${(images.length >= 1 ? styles["has-picture"] : styles["no-picture"])}`}>
             <div className={styles["preview-image-container"]}>
                 <div className={styles["first-preview-image"]}>
-                    {files.filter((file, index) => index === 0).map((file, index) =>
+                    {images.filter((file, index) => index === 0).map((file, index) =>
                         <img data-index={index} className={styles["preview-image"]} src={file} onClick={removeFile} alt=""/>
                     )}
                 </div>
                 <div className={styles["preview-images-right"]}>
-                    {files.filter((file, index) => index >= 1).map((file, index) =>
+                    {images.filter((file, index) => index >= 1).map((file, index) =>
                         <img data-index={index} className={styles["preview-image"]} src={file} onClick={removeFile} alt=""/>
                     )}
                 </div>
