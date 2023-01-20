f411f89ab53b06e750c48a92bb24be6a7e631ad0
Hausch Johannes <jh285@hdm-stuttgart.de>
2023-01-20 22:11:54 +0100

    Add ratings and error handling

--- react-frontend/src/recipe-full-view/rating-section/RatingSection.js
+++ react-frontend/src/recipe-full-view/rating-section/RatingSection.js
@@ -1,30 +1,51 @@
 import styles from './RatingSection.module.css';
-import {useParams} from "react-router-dom";
-import {useGetRatingsPage} from "../../modules/api";
+import {useParams} from 'react-router-dom';
+import {useGetRatingsPage} from '../../modules/api';
+import Throbber from '../../modules/throbber/throbber';
+import Modal from '../../modules/Modal/Modal';
 
-export default function RatingSection(){
+export default function RatingSection() {
+	let {recipeId} = useParams();
+	let [ratingList, error] = useGetRatingsPage(recipeId, {});
 
-    let { recipeId } = useParams();
-    let [ratingContainer, error] = useGetRatingsPage(recipeId,{});
+	console.log(ratingList);
 
-    let rating;
+	let getRating = () => {
+		if (ratingList) {
+            return ratingList.ratings.map((element) => {
+				return (
+					<div className={styles['comment-container']}>
+						<div className={styles['comment-left']}>{element.userId}</div>
+						<div className={styles['comment-right']}>
+							<div>
+								<span className="material-symbols-outlined">star</span>
+								{element.ratingValue}/5
+							</div>
+							<div>{element.comment}</div>
+						</div>
+					</div>
+				);
+			});
+		} else {
+			return <Throbber />;
+		}
+	};
 
-    if (ratingContainer){
-        rating = ratingContainer.ratings.map((element) => {
-            return (<div className={styles["comment-container"]}>
-                <div className={styles["comment-left"]}>
-                    {element.toString()}
-                </div>
-                <div className={styles["comment-right"]}>
-                    Sollte rechts sein!
-                </div>
-            </div>)
-        })
-    }
-
-    return (
-        <div>
-            {rating}
-        </div>
-    )
+	if (!error) {
+		return <div>{getRating()}</div>;
+	} else if (error.includes('404')) {
+		return (
+			<div>
+				Für dieses Rezept gibt es bisher keine Bewertungen.
+				<br />
+				HIER KÖNNTE IHRE BEWERTUNG STEHEN!
+			</div>
+		);
+	} else {
+		return (
+			<Modal className="error" show={true}>
+				{error}
+			</Modal>
+		);
+	}
 }
