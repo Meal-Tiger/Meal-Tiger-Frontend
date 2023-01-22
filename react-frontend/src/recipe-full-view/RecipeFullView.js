d8bb526fe47e5ac1d1683581feec29ea33b55c05
ds217 <ds217@hdm-stuttgart.de>
2023-01-22 23:25:18 +0100

    image slider modal box #200

--- react-frontend/src/recipe-full-view/RecipeFullView.js
+++ react-frontend/src/recipe-full-view/RecipeFullView.js
@@ -2,11 +2,15 @@ import RatingEditor from "./rating-editor/RatingEditor";
 import ImageSlider from "./image-slider/ImageSlider";
 import RecipeDescription from "./recipe-description/RecipeDescription";
 import RatingSection from "./rating-section/RatingSection";
+import Modal from "../modules/Modal/Modal";
+import {useState} from "react";
 
 export default function RecipeFullView(){
+    let [showModal, setShowModal] = useState(false);
     return(
         <div>
-            <ImageSlider/>
+            <ImageSlider showModal={showModal} setShowModal={setShowModal}/>
+            <Modal show={showModal} setShow={setShowModal} className={"image-modal"}> <ImageSlider noLinearGrid={true}/></Modal>
             <RecipeDescription/>
             <RatingEditor/>
             <RatingSection/>
