1d97383abee5f7db5c790753e8b4743b2c6203c4
Kay Knöpfle <kay.knoepfle@gmx.de>
2023-01-31 14:39:30 +0100

    implemented pp placeholder

--- react-frontend/src/recipe-overview/RecipeCard/RecipeCard.js
+++ react-frontend/src/recipe-overview/RecipeCard/RecipeCard.js
@@ -4,33 +4,33 @@ import styles from './RecipeCard.module.css';
 
 export default function RecipeCard(props) {
 
-	if(props.images.length > 0){
+	console.log(props.user);
+
+	if (props.images.length > 0) {
 		return (
 			<div onClick={props.onClick} className={styles['recipe-card']}>
-			<div
-				title={props.title}
-				className={`${styles['recipe-card-img']}`}
-				style={{backgroundImage: `url(${getImageUrl(props.images[0])}`}}
-			></div>
-			<div className={styles['recipe-card-infobox']}>
-				<h3 className="titel">{props.title}</h3>
-				<div className={styles['recipe-card-info']}>
-					<span className="material-symbols-outlined">schedule</span>
-					{getFormatedTime(props.time)}
-				</div>
-				<Difficulty difficulty={props.difficulty}/>
-				<div className={` ${styles['recipe-card-info']} ${styles.user}`}>
-					<div className={` ${props.user.profilePictureId !== null ? styles.user : "" }`} style={{backgroundImage: `url(${getImageUrl(props.user.profilePictureId)}`}}></div> <span>{props.user.username}</span>
-				</div>
-				<div className={`${styles['recipe-card-info']} ${'rating'}`}>
-					<span className={'material-symbols-outlined'}>star</span>
-					{props.rating}/5
+				{/* Rezeptbild */}
+				<div title={props.title} className={`${styles['recipe-card-img']}`} style={{backgroundImage: `url(${getImageUrl(props.images[0])}`}}></div>
+				{/* Infobox */}
+				<div className={styles['recipe-card-infobox']}>
+					<h3 className="titel">{props.title}</h3>
+					<div className={styles['recipe-card-info']}>
+						<span className="material-symbols-outlined">schedule</span>
+						{getFormatedTime(props.time)}
+					</div>
+					<Difficulty difficulty={props.difficulty} />
+					<div className={` ${styles['recipe-card-info']} ${styles.user}`}>
+						{/* User */}
+						<div className={` ${styles.user}`} style={{backgroundImage: `url(${getImageUrl(props.user.profilePictureId)}`}}></div> <span>{props.user.username}</span>
+					</div>
+					<div className={`${styles['recipe-card-info']} ${'rating'}`}>
+						<span className={'material-symbols-outlined'}>star</span>
+						{props.rating}/5
+					</div>
 				</div>
 			</div>
-		</div>
-		)
-	}
-	else {
+		);
+	} else {
 		return (
 			<div onClick={props.onClick} className={styles['recipe-card']}>
 				<div className={styles['recipe-card-infobox']}>
@@ -39,9 +39,10 @@ export default function RecipeCard(props) {
 						<span className="material-symbols-outlined">schedule</span>
 						{getFormatedTime(props.time)}
 					</div>
-					<Difficulty difficulty={props.difficulty}/>
+					<Difficulty difficulty={props.difficulty} />
 					<div className={` ${styles['recipe-card-info']} ${styles.user}`}>
-						<img className={styles.user} src={getImageUrl(props.user.imageId)} alt=""></img> {props.user.username}
+						{/* User */}
+						<div className={` ${styles.user}`} style={{backgroundImage: `url(${getImageUrl(props.user.profilePictureId)}`}}></div> <span>{props.user.username}</span>
 					</div>
 					<div className={`${styles['recipe-card-info']} ${'rating'}`}>
 						<span className={'material-symbols-outlined'}>star</span>
@@ -51,5 +52,4 @@ export default function RecipeCard(props) {
 			</div>
 		);
 	}
-
 }
