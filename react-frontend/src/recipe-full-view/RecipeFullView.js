0de6818915ed5344ebe54cc6c253f8ceafe673f7
ds217 <ds217@hdm-stuttgart.de>
2023-02-01 11:35:08 +0100

    add optional close button to modal #281

--- react-frontend/src/recipe-full-view/RecipeFullView.js
+++ react-frontend/src/recipe-full-view/RecipeFullView.js
@@ -10,7 +10,7 @@ export default function RecipeFullView(){
     return(
         <div>
             <ImageSlider showModal={showModal} setShowModal={setShowModal}/>
-            <Modal show={showModal} setShow={setShowModal} className={"image-modal"}> <ImageSlider showModal={showModal} noLinearGrid={true}/></Modal>
+            <Modal show={showModal} setShow={setShowModal} className={"image-modal"} closeButton={true}> <ImageSlider showModal={showModal} noLinearGrid={true} /></Modal>
             <RecipeDescription/>
             <RatingEditor/>
             <RatingSection/>
