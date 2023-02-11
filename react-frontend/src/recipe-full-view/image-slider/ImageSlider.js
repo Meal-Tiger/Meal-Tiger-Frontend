9e330e2af937b41d3a3819c320667721144c4c79
ds217 <ds217@hdm-stuttgart.de>
2023-02-11 16:08:11 +0100

    show ImageSlider only if recipe has images #246

--- react-frontend/src/recipe-full-view/image-slider/ImageSlider.js
+++ react-frontend/src/recipe-full-view/image-slider/ImageSlider.js
@@ -2,18 +2,13 @@ import styles from './ImageSlider.module.css';
 
 import {useEffect, useState} from 'react';
 
-import {useParams} from 'react-router-dom';
-import {getImageUrl, useGetRecipe} from '../../modules/api';
-import Throbber from '../../modules/throbber/throbber';
+import {getImageUrl} from '../../modules/api';
 import {useDrag} from "@use-gesture/react";
 
 export default function ImageSlider(pros) {
 
 	let showModal = pros.showModal;
 	let setShowModal = pros.setShowModal;
-
-	let {recipeId} = useParams();
-	let [recipe] = useGetRecipe(recipeId);
 
 	const [currentIndex, setCurrentIndex] = useState(0);
 
@@ -23,10 +18,7 @@ export default function ImageSlider(pros) {
 	});
 
 	let getSlides = () => {
-		if (recipe)
-			return recipe.images.map((element) =>  getImageUrl(element));
-		else
-			return [];
+		return pros.recipe.images.map((element) =>  getImageUrl(element));
 	};
 
 	const switchToNextIndex = () => {
@@ -71,20 +63,16 @@ export default function ImageSlider(pros) {
 		}
 	}, {swipe: {duration: 2000}, axis: "x"});
 
-	if (recipe) {
-		return (
-			<div className={styles['slider-container']} onClick={setShowModal}>
-				<div className={styles.slider}>
-					<div {...dragBind()} className={styles.slide} style={slideImage}>
-						<div className={styles['title-container']}>
-							<h1 className={styles.title}>{recipe.title}</h1>
-						</div>
-						<div className={styles['slider-buttons']}>{setSliderButtons}</div>
+	return (
+		<div className={styles['slider-container']} onClick={setShowModal}>
+			<div className={styles.slider}>
+				<div {...dragBind()} className={styles.slide} style={slideImage}>
+					<div className={styles['title-container']}>
+						<h1 className={styles.title}>{pros.recipe.title}</h1>
 					</div>
+					<div className={styles['slider-buttons']}>{setSliderButtons}</div>
 				</div>
 			</div>
-		);
-	} else {
-		return <Throbber />;
-	}
+		</div>
+	);
 }
